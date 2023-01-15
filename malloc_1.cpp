5510c4a3054aa388e6ad151f698332274f4c2d63
zivdamir <73081641+zivdamir@users.noreply.github.com>
2023-01-15 19:04:14 +0200

    Create malloc_1.cpp

---
+++ malloc_1.cpp
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void* smalloc(size_t size)
+{
+    if(size == 0 || size > 1e8)
+	{
+		return NULL;
+	}
+	
+	void* before_malloc_program_break = sbrk(0);
+	void* after_malloc_program_break = sbrk(size);
+	
+	if(after_malloc_program_break != before_malloc_program_break)
+	{
+		return NULL;
+	}
+	
+	return after_malloc_program_break;
+}
